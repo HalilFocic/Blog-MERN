9cd6f7020e7c256f5f3d67044fe7fb469de934a6
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-03 12:10:40 +0100

    Form Basic look

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -27,9 +27,58 @@ const About = () => {
           Mostar
         </div>
       </div>
-      <div className="about">About</div>
-      <div className="interests">Interests</div>
-      <div className="contact-me">Contact me</div>
+      <div className="about">
+        <div class="about-me">
+          <h1 style={{ marginBottom: "2rem", color: "royalblue" }}>About me</h1>
+          <p>
+            I am second year software engineering student. My interests are web
+            development technologies like React and NodeJS. Looking for
+            collaboration/internship/mentor
+          </p>
+        </div>
+        <div className="about-project">
+          <h1
+            style={{
+              marginBottom: "2rem",
+              marginTop: "5rem",
+              color: "royalblue",
+            }}
+          >
+            About project
+          </h1>
+          <p>
+            Blog site project was built using
+            MERN(MongoDB,Express,React,NodeJS). It has 3 main
+            pages,Home,About,Hero Info. On home page you are able to view or
+            create posts. Hero info page will give you main info about superhero
+            you type in. Since you are reading about page you already know what
+            this page is all about :)
+          </p>
+        </div>
+      </div>
+      <div className="contact-me">
+        <form className="contact-me-form">
+          <label htmlFor="email" className="frm-lbl">
+            Email
+          </label>
+          <input type="email" className="frm-input" />
+          <label htmlFor="subject" className="frm-lbl">
+            Subject
+          </label>
+          <input type="text" className="frm-input" />
+          <label htmlFor="emailText" className="frm-lbl">
+            Enter your message
+          </label>
+          <textarea
+            width="50px"
+            className="frm-input frm-textarea"
+            style={{ resize: "none" }}
+          ></textarea>
+          <button type="submit" className="btn-submit">
+            Send Email
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
