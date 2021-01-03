e12a7b58bd37cd6bce48bfe0dbad0e644013efa3
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-03 09:30:35 +0100

    Navbar Style finished

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 const Navbar = () => {
   return (
@@ -7,15 +7,13 @@ const Navbar = () => {
       <div className="nav-title">Blog Site</div>
       <ul className="nav-list">
         <li className="nav-item">
-          <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li className="nav-item">
-          <Link to="/heroinfo">Hero Info</Link>
+          <NavLink to="/heroinfo">Hero Info</NavLink>
         </li>
         <li className="nav-item">
-          <Link to="/about">
-            <a className="link">About</a>
-          </Link>
+          <NavLink to="/about">About</NavLink>
         </li>
       </ul>
     </nav>
