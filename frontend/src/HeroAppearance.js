b02072fd0b2634a91a2f7f76555cd7722e5ae6cc
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-04 22:12:05 +0100

    Hero Appearance styled

--- frontend/src/HeroAppearance.js
+++ frontend/src/HeroAppearance.js
@@ -4,12 +4,24 @@ const HeroAppearance = (data) => {
   return (
     <>
       <img src={data.profile} alt="profile" className="hero-picture"></img>
-      <div className="hero-item">{data["gender"]}</div>
-      <div className="hero-item">{data["race"]}</div>
-      <div className="hero-item">{data["height"][1]}</div>
-      <div className="hero-item">{data["weight"][1]}</div>
-      <div className="hero-item">{data["hair-color"]}</div>
-      <div className="hero-item">{data["eye-color"]}</div>
+      <div className="hero-item">
+        Gender:<span className="blue-text">{data["gender"]}</span>
+      </div>
+      <div className="hero-item">
+        Race:<span className="blue-text">{data["race"]}</span>
+      </div>
+      <div className="hero-item">
+        Height:<span className="blue-text">{data["height"][1]}</span>
+      </div>
+      <div className="hero-item">
+        Weight:<span className="blue-text">{data["weight"][1]}</span>
+      </div>
+      <div className="hero-item">
+        Hair color:<span className="blue-text">{data["hair-color"]}</span>
+      </div>
+      <div className="hero-item">
+        Eye color:<span className="blue-text">{data["eye-color"]}</span>
+      </div>
     </>
   );
 };
