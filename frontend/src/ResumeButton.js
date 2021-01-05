6e67608e8d115dffe11e73f1c6e39afa16e9cd71
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-05 21:29:56 +0100

    ResumeButton.js

---
+++ frontend/src/ResumeButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+// Simple button that will export cv in pdf when clicked!
+const ResumeButton = () => {
+  return <div>Resume button</div>;
+};
+
+export default ResumeButton;
