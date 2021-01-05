6b3e85f837360336cdb5f4983d86826ee85733b2
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-05 21:27:07 +0100

    Post.js schema

---
+++ frontend/src/Post.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Post component
+// It will take arguemnts like author,text,content,like number etc
+
+const Post = () => {
+  return <div>Post</div>;
+};
+
+export default Post;
