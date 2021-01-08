0cb5cb4cd651d0818f9b5384bea3f508e0f1a6c2
HalilCodes <56510805+HalilCodes@users.noreply.github.com>
2021-01-08 12:37:39 +0100

    fetchPosts method

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+
 const Home = () => {
-  return <h1>Home page!</h1>;
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const fetchPosts = async () => {
+    const response = await fetch("/posts");
+    const data = await response.json();
+    setPosts(data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchPosts();
+  });
+  return (
+    <>
+      <div className="home-container">
+        {loading ? <h1>Loading...</h1> : <h1>Posts</h1>}
+      </div>
+    </>
+  );
 };
 export default Home;
